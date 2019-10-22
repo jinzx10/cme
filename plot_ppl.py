0fc35d2d60636d9c050122750c8fc3d21f2e213d
jinzx10 <jzx016@hotmail.com>
2019-10-22 18:43:10 -0400

    tq

--- plot_ppl.py
+++ plot_ppl.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-state = np.loadtxt('x_t.txt')
+state = np.loadtxt('state_E0002.txt')
+x = np.loadtxt('x_E0002.txt')
 
 state_avg = np.mean(state, axis = 1)
+x_avg = np.mean(x, axis = 1)
 
-plt.plot(state_avg)
+num_1 = np.sum(x>2.6, axis=1)
+num_0 = np.sum(x<=2.6, axis=1)
+
+plt.subplot(1,2,1)
+plt.plot(1-state_avg)
+plt.subplot(1,2,2)
+plt.plot(num_0/960)
+
 plt.show()
+#plt.savefig('state0.png', format='png', dpi=300)
